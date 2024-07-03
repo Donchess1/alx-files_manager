12f00706b2baf7d4dec8c35a0466df8484c89015
Donchess1 <Stepabod@yahoo.com>
2024-07-03 21:20:37 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -62,14 +62,14 @@ userQueue.process(async (job, done) => {
   }
   console.log(`Welcome ${user.email}!`);
   try {
-    const mailSubject = 'Welcome to ALX-Files_Manager by B3zaleel';
+    const mailSubject = 'Welcome to the team Rita and Stephen';
     const mailContent = [
       '<div>',
       '<h3>Hello {{user.name}},</h3>',
-      'Welcome to <a href="https://github.com/B3zaleel/alx-files_manager">',
+      'Welcome to <a href="https://github.com/Donchess1/alx-files_manager">',
       'ALX-Files_Manager</a>, ',
-      'a simple file management API built with Node.js by ',
-      '<a href="https://github.com/B3zaleel">Bezaleel Olakunori</a>. ',
+      'a simple file management API built with Node.js by Ritapossible',
+      '<a href="https://https://github.com/Ritapossible</a>. ',
       'We hope it meets your needs.',
       '</div>',
     ].join('');
